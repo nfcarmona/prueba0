dff39787f48e7a54316a17d500a74383243ca1ce
nfcarmona <nicocarmona2015@gmail.com>
2021-09-19 14:02:23 -0500

    1er java scrip

---
+++ js/primer_java_script_10.js
@@ -0,0 +1,14 @@
+//alert("mensaje en la carpeta de js")
+
+var numero1=4878;
+var numero2=4.343
+var nombre="Pepito";
+var masculino=true;
+var edad="23";
+
+//alert("El nombre "+nombre+" la edad es: "+edad);
+// Realizar codigo de sumatoria de dos numeros 
+var numero1= prompt("ingrese numero 1");
+var numero2= prompt("ingrese numero 2");
+var sumar = parseInt(numero1)+parseInt(numero2);
+alert("El valor de la suma es: "+sumar);
